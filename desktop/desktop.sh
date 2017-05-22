703d073ca63b2416c6cc7ddb0bc6d808f446355f
MattDamon <pstrussel@protonMail.com>
2017-05-22 14:36:31 -0500

    Added Var Comments to desktop.sh

---
+++ desktop/desktop.sh
@@ -0,0 +1,8 @@
+#################################################
+# Takes in input variables to make desktop item #
+#    EXEC = Command/App to be run               #
+#    NAME = Name displayed on desktop           #
+#    ICON = Path/To/Icon                        #
+#  ../Desktop_File.txt template desktop item    #
+#################################################
+
